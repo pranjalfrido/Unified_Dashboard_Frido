a5c1f404083849184b4e070f912ee3ab6c536eb8
Pranjal Tripati <pranjal.t@myfrido.com>
2026-07-27 12:27:14 +0530

    Split bundle into chunks to suppress Vite chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'recharts': ['recharts'],
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001'
